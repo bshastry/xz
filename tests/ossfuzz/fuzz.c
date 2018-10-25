5daf7b7c36ac989b908180aa7abed75b22e0f69c
Bhargava Shastry <bshastry@sect.tu-berlin.de>
2018-10-25 11:48:11 +0200

    ossfuzz: Addressed comments by Lasse Collin dated 24.10.18

--- tests/ossfuzz/fuzz.c
+++ tests/ossfuzz/fuzz.c
@@ -9,55 +9,24 @@
 static bool
 init_decoder(lzma_stream *strm)
 {
-    // Initialize a .xz decoder. The decoder supports a memory usage limit
-    // and a set of flags.
-    //
-    // The memory usage of the decompressor depends on the settings used
-    // to compress a .xz file. It can vary from less than a megabyte to
-    // a few gigabytes, but in practice (at least for now) it rarely
-    // exceeds 65 MiB because that's how much memory is required to
-    // decompress files created with "xz -9". Settings requiring more
-    // memory take extra effort to use and don't (at least for now)
-    // provide significantly better compression in most cases.
-    //
-    // Memory usage limit is useful if it is important that the
-    // decompressor won't consume gigabytes of memory. The need
-    // for limiting depends on the application. In this example,
-    // no memory usage limiting is used. This is done by setting
-    // the limit to UINT64_MAX.
-    //
-    // The .xz format allows concatenating compressed files as is:
-    //
-    //     echo foo | xz > foobar.xz
-    //     echo bar | xz >> foobar.xz
-    //
-    // When decompressing normal standalone .xz files, LZMA_CONCATENATED
-    // should always be used to support decompression of concatenated
-    // .xz files. If LZMA_CONCATENATED isn't used, the decoder will stop
-    // after the first .xz stream. This can be useful when .xz data has
-    // been embedded inside another file format.
-    //
-    // Flags other than LZMA_CONCATENATED are supported too, and can
-    // be combined with bitwise-or. See lzma/container.h
-    // (src/liblzma/api/lzma/container.h in the source package or e.g.
-    // /usr/include/lzma/container.h depending on the install prefix)
-    // for details.
+    /* Initialize lzma stream decoder, setting a memory limit of 500 MB,
+     *  and setting the LZMA_IGNORE_CHECK flag which instructs the
+     *  decoder to disable CRC checks on compressed data.
+     */
     lzma_ret ret = lzma_stream_decoder(
-            strm, UINT64_MAX, LZMA_CONCATENATED);
+            strm, /* memory limit */ 500 << 20,
+            LZMA_CONCATENATED | LZMA_IGNORE_CHECK);
 
-    // Return successfully if the initialization went fine.
     if (ret == LZMA_OK)
         return true;
 
-#ifdef __DEBUG__
-    // Something went wrong. The possible errors are documented in
-    // lzma/container.h (src/liblzma/api/lzma/container.h in the source
-    // package or e.g. /usr/include/lzma/container.h depending on the
-    // install prefix).
-    //
-    // Note that LZMA_MEMLIMIT_ERROR is never possible here. If you
-    // specify a very tiny limit, the error will be delayed until
-    // the first headers have been parsed by a call to lzma_code().
+    /* The flag "FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION" is defined
+     * by the fuzzer build script. We use it here to disable debug
+     * messages. Disabling debug messages in fuzzer test harnesses seems
+     *  to be the norm for oss-fuzz targets.
+     */
+#ifndef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
+    // Something went wrong, print an informative debug message
     const char *msg;
     switch (ret) {
     case LZMA_MEM_ERROR:
@@ -69,12 +38,6 @@ init_decoder(lzma_stream *strm)
         break;
 
     default:
-        // This is most likely LZMA_PROG_ERROR indicating a bug in
-        // this program or in liblzma. It is inconvenient to have a
-        // separate error message for errors that should be impossible
-        // to occur, but knowing the error code is important for
-        // debugging. That's why it is good to print the error code
-        // at least when there is no good error message to show.
         msg = "Unknown error, possibly a bug";
         break;
     }
@@ -91,16 +54,6 @@ decompress(lzma_stream *strm, const uint8_t *inbuf, size_t inlen,
 {
     size_t remainlen = inlen;
 
-    // When LZMA_CONCATENATED flag was used when initializing the decoder,
-    // we need to tell lzma_code() when there will be no more input.
-    // This is done by setting action to LZMA_FINISH instead of LZMA_RUN
-    // in the same way as it is done when encoding.
-    //
-    // When LZMA_CONCATENATED isn't used, there is no need to use
-    // LZMA_FINISH to tell when all the input has been read, but it
-    // is still OK to use it if you want. When LZMA_CONCATENATED isn't
-    // used, the decoder will stop after the first .xz stream. In that
-    // case some unused data may be left in strm->next_in.
     lzma_action action = LZMA_RUN;
 
     strm->next_in = NULL;
@@ -119,16 +72,15 @@ decompress(lzma_stream *strm, const uint8_t *inbuf, size_t inlen,
             strm->avail_in = (remainlen > BUFSIZ) ? BUFSIZ : remainlen;
             remainlen -= strm->avail_in;
 
-            // Once the end of the input file has been reached,
-            // we need to tell lzma_code() that no more input
-            // will be coming. As said before, this isn't required
-            // if the LZMA_CONCATENATED flag isn't used when
-            // initializing the decoder.
             if (remainlen == 0)
                 action = LZMA_FINISH;
         }
 
         lzma_ret ret = lzma_code(strm, action);
+        /* LZMA_PROG_ERROR should be rarely, if ever, happen
+         * The assertion codifies this expectation.
+         */
+        assert(ret != LZMA_PROG_ERROR);
 
         // TODO: Is this code trying to overwrite outbuf when outlen
         // is exhausted? If so, is that okay?
@@ -138,27 +90,10 @@ decompress(lzma_stream *strm, const uint8_t *inbuf, size_t inlen,
         }
 
         if (ret != LZMA_OK) {
-            // Once everything has been decoded successfully, the
-            // return value of lzma_code() will be LZMA_STREAM_END.
-            //
-            // It is important to check for LZMA_STREAM_END. Do not
-            // assume that getting ret != LZMA_OK would mean that
-            // everything has gone well or that when you aren't
-            // getting more output it must have successfully
-            // decoded everything.
             if (ret == LZMA_STREAM_END)
                 return true;
 
-#ifdef __DEBUG__
-            // It's not LZMA_OK nor LZMA_STREAM_END,
-            // so it must be an error code. See lzma/base.h
-            // (src/liblzma/api/lzma/base.h in the source package
-            // or e.g. /usr/include/lzma/base.h depending on the
-            // install prefix) for the list and documentation of
-            // possible values. Many values listen in lzma_ret
-            // enumeration aren't possible in this example, but
-            // can be made possible by enabling memory usage limit
-            // or adding flags to the decoder initialization.
+#ifndef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
             const char *msg;
             switch (ret) {
             case LZMA_MEM_ERROR:
@@ -171,20 +106,6 @@ decompress(lzma_stream *strm, const uint8_t *inbuf, size_t inlen,
                 break;
 
             case LZMA_OPTIONS_ERROR:
-                // For example, the headers specify a filter
-                // that isn't supported by this liblzma
-                // version (or it hasn't been enabled when
-                // building liblzma, but no-one sane does
-                // that unless building liblzma for an
-                // embedded system). Upgrading to a newer
-                // liblzma might help.
-                //
-                // Note that it is unlikely that the file has
-                // accidentally became corrupt if you get this
-                // error. The integrity of the .xz headers is
-                // always verified with a CRC32, so
-                // unintentionally corrupt files can be
-                // distinguished from unsupported files.
                 msg = "Unsupported compression options";
                 break;
 
@@ -193,18 +114,11 @@ decompress(lzma_stream *strm, const uint8_t *inbuf, size_t inlen,
                 break;
 
             case LZMA_BUF_ERROR:
-                // Typically this error means that a valid
-                // file has got truncated, but it might also
-                // be a damaged part in the file that makes
-                // the decoder think the file is truncated.
-                // If you prefer, you can use the same error
-                // message for this as for LZMA_DATA_ERROR.
                 msg = "Compressed file is truncated or "
                         "otherwise corrupt";
                 break;
 
             default:
-                // This is most likely LZMA_PROG_ERROR.
                 msg = "Unknown error, possibly a bug";
                 break;
             }
@@ -230,14 +144,14 @@ int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     // Init decoder.
     if (!init_decoder(&strm)) {
         // Decoder initialization failed. There's no point
-        // to retry it so we need to exit.
+        // retrying, so bail out.
         return 0;
     }
 
     uint8_t outbuf[BUFSIZ];
 
     if (!decompress(&strm, data, size, outbuf, BUFSIZ)) {
-#ifdef __DEBUG__
+#ifndef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
         fprintf(stderr, "Decode failure\n");
 #endif
     }
